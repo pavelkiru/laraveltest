81328f6e2e0ddc0dd1266fb3db1aebd852ee0c79
pavelkiru <pavelkirushchenko@gmail.com>
2024-02-09 13:02:50 +0300

    next 29

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+
+        Category::factory(10)->create();
+        $tags = Tag::factory(20)->create();
+        $posts = Post::factory(100)->create();
+
+        foreach ( $posts as $post ) {
+            $tagsIds = $tags->random(5)->pluck('id');
+            $post->tags()->attach($tagsIds);
+        }
+
+
+
         // \App\Models\User::factory(10)->create();
     }
 }
